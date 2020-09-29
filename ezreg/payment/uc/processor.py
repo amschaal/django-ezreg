c32da976c9263e70efbb4aba8dbdd07bbc6a1643
adam <amschaal@gmail.com>
2020-09-29 09:11:35 -0700

    export phone for uc processor

--- ezreg/payment/uc/processor.py
+++ ezreg/payment/uc/processor.py
@@ -5,7 +5,7 @@ from collections import OrderedDict
 class UCPaymentProcessor(BasePaymentProcessor):
     id = 'uc_payment_processor'
     name = 'UC Account String Processor'
-    exportable_fields = OrderedDict([('uc','UC'),('account','Account'),('financial_contact_first_name','Financial Contact First Name'),('financial_contact_last_name','Financial Contact Last Name'),('financial_contact_email','Financial Contact Email'),('pi_first_name','PI First Name'),('pi_last_name','PI Last Name'),('pi_email','PI Email')])
+    exportable_fields = OrderedDict([('uc','UC'),('account','Account'),('financial_contact_first_name','Financial Contact First Name'),('financial_contact_last_name','Financial Contact Last Name'),('financial_contact_email','Financial Contact Email'),('financial_contact_phone','Financial Contact Phone'),('pi_first_name','PI First Name'),('pi_last_name','PI Last Name'),('pi_email','PI Email')])
     @staticmethod
     def get_form(data=None, request=None):
         return PaymentForm
