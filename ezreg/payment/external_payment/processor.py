5516a7916c524af73f695de9685231dc0a8934ce
Adam Schaal <amschaal@gmail.com>
2023-02-10 13:55:26 -0800

    issue #188 external payments

---
+++ ezreg/payment/external_payment/processor.py
@@ -0,0 +1,21 @@
+from ezreg.payment.base import BasePaymentProcessor
+from ezreg.payment.external_payment.forms import PaymentForm, ExternalPaymentConfigurationForm
+
+class ExternalPaymentProcessor(BasePaymentProcessor):
+    id = 'external_payment_processor'
+    name = 'External payment processor'
+    exportable_fields = {'external_id':'External ID'}
+    @staticmethod
+    def get_form(data=None, request=None):
+        return PaymentForm
+    @staticmethod
+    def get_configuration_form():
+        return ExternalPaymentConfigurationForm
+    @staticmethod
+    def post_process_form(payment, data):
+        from ezreg.models import Registration, Payment
+        payment.registration.status = Registration.STATUS_REGISTERED
+        payment.status = Payment.STATUS_PENDING
+        payment.save()
+        payment.registration.save()
+    
