c467d75e8e3747c07f73c2e694ec30a2e89096f9
Adam Schaal <amschaal@gmail.com>
2023-02-10 16:27:30 -0800

    issue #188

--- ezreg/payment/external_payment/processor.py
+++ ezreg/payment/external_payment/processor.py
@@ -5,6 +5,7 @@ class ExternalPaymentProcessor(BasePaymentProcessor):
     id = 'external_payment_processor'
     name = 'External payment processor'
     exportable_fields = {'external_id':'External ID'}
+    use_external_url = True
     @staticmethod
     def get_form(data=None, request=None):
         return PaymentForm
@@ -15,7 +16,10 @@ class ExternalPaymentProcessor(BasePaymentProcessor):
     def post_process_form(payment, data):
         from ezreg.models import Registration, Payment
         payment.registration.status = Registration.STATUS_REGISTERED
-        payment.status = Payment.STATUS_PENDING
+        payment.status = Payment.STATUS_UNPAID
         payment.save()
         payment.registration.save()
+    @staticmethod
+    def get_additional_email_text(payment):
+        return 'Please ensure that you have paid for your registration at {}.  Any unpaid registrations are subject to cancellation'.format(payment.registration.event.outside_url)
     
